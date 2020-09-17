af0fc049979b630131acf319824636f9306032e8
bolicoding <bolicoding@gmail.com>
2020-09-17 11:31:08 -0600

    String/string values/0 based index

--- variable/main.py
+++ variable/main.py
@@ -1,5 +1,19 @@
-sentence = 'The quick brown fox jumped over the lazy dog'
-sentence_two = sentence.upper()
+# sentence = 'The quick brown fox jumped over the lazy dog'
+# sentence_two = sentence.upper()
 
-print(sentence)
-print(sentence_two)
+# (sentence)
+# print(sentence_two)
+
+# The quick brown fox jumped
+# T => 0
+# h => 1
+# e => 2
+# ' ' => 3
+
+# you cannot change content inside of a string
+
+starter_sentence = 'The quick brown fox jumped'
+
+new_sentence = 'Thy' + starter_sentence[3:]
+
+print(new_sentence)
