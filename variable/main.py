8070b06c7ca8733035574c1f54ad73d52f5b424e
bolicoding <bolicoding@gmail.com>
2020-09-17 11:56:54 -0600

    Heredoc overview

--- variable/main.py
+++ variable/main.py
@@ -12,8 +12,20 @@
 
 # you cannot change content inside of a string
 
-starter_sentence = 'The quick brown fox jumped'
+#starter_sentence = 'The quick brown fox jumped'
 
-new_sentence = 'Thy' + starter_sentence[3:]
+#new_sentence = 'Thy' + starter_sentence[3:]
 
-print(new_sentence)
+#print(new_sentence)
+
+# Heredoc
+
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip()
+
+print(content)
