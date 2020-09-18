b016adbc977fb2ce2641aa96f2c66614039b423c
bolicoding <bolicoding@gmail.com>
2020-09-18 14:38:51 -0600

    Python lists overview

--- bootcamp.py
+++ bootcamp.py
@@ -1,3 +1,12 @@
-calculation = 8 + 2 * 5 - (9+2) ** 2
+#calculation = 8 + 2 * 5 - (9+2) ** 2
 
-print(calculation)
+#print(calculation)
+from functools import reduce
+
+def manual_exponent(base_num, pow_num):
+    result = 1
+    for num in range(pow_num):
+        result = result * base_num
+    return result
+
+print(manual_exponent(2, 3))
---
+++ python-lists.py
@@ -0,0 +1,24 @@
+"""
+User Database Query
+Mili
+Ashley
+Chelsey
+"""
+
+users = ['Mili','Ashley','Chelsey']
+
+print(users)
+
+users.insert(0, 'Boli')
+
+print(users)
+
+users.append('Jr')
+
+print(users)
+
+print(users[2])
+
+users[4] = 'Gera'
+
+print(users)
