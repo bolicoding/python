d4a79913edc11e622287eff26b6f98b188058296
bolicoding <bolicoding@gmail.com>
2020-09-18 15:44:04 -0600

    Mixed list overview

--- python-lists.py
+++ python-lists.py
@@ -24,15 +24,30 @@ Chelsey
 #print(users)
 
 
-users = ['Mili','Ashley','Chelsey']
-print(users)
+#users = ['Mili','Ashley','Chelsey']
+#print(users)
 
-users.remove('Mili')
-print(users)
+#users.remove('Mili')
+#print(users)
 
-popped_user = users.pop()
-print(popped_user)
-print(users)
+#popped_user = users.pop()
+#print(popped_user)
+#print(users)
 
-del users[0]
-print(users)
+#del users[0]
+#print(users)
+
+users = ['Mili','Ashley','Chelsey', 'Boli']
+
+ids = [1, 2, 3, 4]
+
+mixed_list = [42, 10.3, 'Gomes', users]
+
+print(mixed_list)
+
+user_list = mixed_list.pop()
+
+print(user_list)
+print(mixed_list)
+
+nested_list = [[123],[234],[345]]
