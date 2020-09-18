09c1565779607aca7b4d31f0c62ea790773625ef
bolicoding <bolicoding@gmail.com>
2020-09-18 16:47:54 -0600

    sorted list

--- python-lists.py
+++ python-lists.py
@@ -94,7 +94,7 @@ Chelsey
 #tag_range = tags[2:3]
 
 #print(tag_range)
-
+"""
 tags = [
     'python',
     'development',
@@ -103,8 +103,25 @@ tags = [
     'programming',
     'computer science'
 ]
-
+"""
 #tag_range = tags[:-1:2]
-tag_range = tags[::-1]
+#tag_range = tags[::-1]
 
-print(tag_range)
+#print(tag_range)
+
+sale_prices = [
+    100,
+    83,
+    220,
+    40,
+    100,
+    400,
+    10,
+    1,
+    3
+]
+
+sorted_list = sorted(sale_prices)
+
+print(sorted_list)
+print(sale_prices)
