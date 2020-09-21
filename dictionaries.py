cbc47bbbcb3c419f3eaca2f9c189c084f8b7a03a
bolicoding <bolicoding@gmail.com>
2020-09-21 16:01:46 -0600

    weighted lottery

--- dictionaries.py
+++ dictionaries.py
@@ -16,14 +16,39 @@ players = {
 #print(players["LB"])
 #print(players)
 
+"""
+players = {
+    "ST": "Werner",
+    "CB": "Silva",
+    "RW": "Havertz",
+    "CM": "Mount",
+    "LB": "Chilwell"
+}
+
+print(list(players.values())[1])
+
 teams = {
     'barca': ['Messi', 'Suarez', 'Neymar'],
     'chelsea': ['Werner', 'Havertz', 'Mount'],
-    'spurs': ['Kane', 'Son', 'Bale']
+    'spurs': ['Kane', 'Son', 'Bale'],
+    'man city': ['Mahrez', 'Foden', 'Sterling']
+}
+"""
+import random
+def weighted_lottery(weights):
+    container_list = []
+    for (name, weight) in weights.items():
+        for _ in range(weight):
+            container_list.append(name)
+        return random.choice(container_list)
+    
+
+other_weights = {
+    'winning': 1,
+    'break_even': 2,
+    'losing': 3
 }
 
-teams['man city'] = ['Mahrez', 'Foden', 'Sterling']
+weighted_lottery(other_weights)
 
-print(teams['spurs'])
-print(teams)
-print(teams['chelsea'][2])
+print(weighted_lottery(other_weights))
