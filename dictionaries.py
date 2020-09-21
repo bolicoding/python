2497c20098c92d5cc03b6ce37e843886bfd62a8a
bolicoding <bolicoding@gmail.com>
2020-09-21 13:38:56 -0600

    dictionaries/adding items

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,4 @@
+"""
 players = {
     "ST": "Werner",
     "CB": "Silva",
@@ -5,11 +6,24 @@ players = {
     "CM": "Mount",
     "LB": "Chilwell"
 }
+"""
 
-center_back = players["CB"]
-right_wing = players["RW"]
+#center_back = players["CB"]
+#right_wing = players["RW"]
 
-print(center_back)
-print(right_wing)
-print(players["LB"])
-print(players)
+#print(center_back)
+#print(right_wing)
+#print(players["LB"])
+#print(players)
+
+teams = {
+    'barca': ['Messi', 'Suarez', 'Neymar'],
+    'chelsea': ['Werner', 'Havertz', 'Mount'],
+    'spurs': ['Kane', 'Son', 'Bale']
+}
+
+teams['man city'] = ['Mahrez', 'Foden', 'Sterling']
+
+print(teams['spurs'])
+print(teams)
+print(teams['chelsea'][2])
