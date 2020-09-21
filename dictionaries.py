b44571df31cd04fd67bf5aee88043c3be87da217
bolicoding <bolicoding@gmail.com>
2020-09-21 12:07:32 -0600

    Dictionaries overview

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+players = {
+    "ST": "Werner",
+    "CB": "Silva",
+    "RW": "Havertz",
+    "CM": "Mount",
+    "LB": "Chilwell"
+}
+
+center_back = players["CB"]
+right_wing = players["RW"]
+
+print(center_back)
+print(right_wing)
+print(players["LB"])
+print(players)
