934fdf08069d20d861fa5b5a19f0485cf5276c4f
bolicoding <bolicoding@gmail.com>
2020-09-22 10:22:07 -0600

    zip function

--- tuples.py
+++ tuples.py
@@ -14,14 +14,31 @@ post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'p
 
 print(post[1::2])
 """
-post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+#post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
 
 #post = post[:-1]
 
 #post = post[1:]
 
-post = list(post)
-post.remove('published')
-post = tuple(post)
+#post = list(post)
+#post.remove('published')
+#post = tuple(post)
 
-print(post)
+#print(post)
+
+"""
+priority_index = {
+    (1, 'premier'): [1, 34, 12],
+    (1, 'mvp'): [84, 22, 24],
+    (2, 'standard'): [93, 81, 3]
+}
+
+print(list(priority_index.keys()))
+"""
+
+positions = ['cb', 'lb', 'st', 'lw']
+players = ['Silva', 'Chilwell', 'Werner', 'Pulisic']
+
+scoreboard = zip(positions, players)
+
+print(list(scoreboard))
