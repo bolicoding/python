657e1b4e92787cc98afd388ca22090aff7f7653e
bolicoding <bolicoding@gmail.com>
2020-09-22 10:06:12 -0600

    removing from beginning and end and changing data type tuple --> list --> tuple

--- tuples.py
+++ tuples.py
@@ -1,3 +1,4 @@
+"""
 post = ('Python Basics', 'Intro guide to Python', 'Some cool python content')
 
 tags = ['python', 'coding', 'tutorial']
@@ -7,3 +8,20 @@ post += (tags,)
 print(post)
 print(post[-1])
 print(post[-1][1])
+
+
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+print(post[1::2])
+"""
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+#post = post[:-1]
+
+#post = post[1:]
+
+post = list(post)
+post.remove('published')
+post = tuple(post)
+
+print(post)
