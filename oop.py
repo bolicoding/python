fb178b93bf5293e85f6f8ec3fa107bd7ebee88a8
bolicoding <bolicoding@gmail.com>
2020-09-25 13:45:33 -0600

    dunder init and dunder string

--- oop.py
+++ oop.py
@@ -29,7 +29,7 @@ snapchat = Invoice('SnapChat', 200)
 print(google.formatter())
 print(snapchat.formatter())
 """
-
+"""
 class Invoice:
     
     def __init__(self, client, total):
@@ -47,3 +47,44 @@ print(google.client)
 google.client = 'Yahoo'
 
 print(google.client)
+"""
+"""
+class Invoice:
+    
+    def __init__(self, client, total):
+        self._client = client 
+        self._total = total
+        
+    def formatter(self):
+        return f'{self._client} owes: ${self._total}'
+    
+    @property
+    def client(self):
+        return self._client
+    
+    @client.setter
+    def client(self, client):
+        self._client = client
+    
+    @property
+    def total(self):
+        return self._total 
+       
+google = Invoice('Google', 100)
+print(google.client)
+
+google.client = 'Yahoo'
+
+print(google.client)
+"""
+
+class Invoice:
+    def __init__(self, client, total):
+        self.client = client
+        self.total = total
+    
+    def __str__(self):
+        return 'Invoice from {self.client} for {self.total}'
+    
+inv = Invoice('Google', 500)
+print(str(inv))
